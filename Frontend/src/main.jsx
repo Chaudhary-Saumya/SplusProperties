2d65b9a400866bf561d3ef663f31eab474ecb5f1
Saumya Chaudhary <144538655+Chaudhary-Saumya@users.noreply.github.com>
2026-05-27 10:33:52 +0530

    Add Vercel Analytics

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,17 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { AuthProvider } from './context/AuthContext.jsx'
-import { GoogleOAuthProvider } from '@react-oauth/google';
 
-import { HelmetProvider } from 'react-helmet-async';
+import { AuthProvider } from './context/AuthContext.jsx'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+import { HelmetProvider } from 'react-helmet-async'
+import { Analytics } from '@vercel/analytics/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
-      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID || 'dummy-client-id'}>
+      <GoogleOAuthProvider
+        clientId={
+          import.meta.env.VITE_GOOGLE_CLIENT_ID || 'dummy-client-id'
+        }
+      >
         <AuthProvider>
           <App />
+          <Analytics />
         </AuthProvider>
       </GoogleOAuthProvider>
     </HelmetProvider>
